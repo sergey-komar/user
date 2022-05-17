0855c8aa05648eb28513f21326f15b2465a3235f
sergey-komar <kursev81@yandex.ru>
2022-05-17 19:13:50 +0300

    work

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
 
-})
+    $('.questions-accardion__btn').on('click', function(){
+        $(this).next().slideToggle(500); 
+     });
+
+     $('.specialists-block__button').on('click', function(){
+        $('.specialists-block__inner--none').slideToggle(); 
+    });
+    $('#my-block').slideToggle('medium', function() { if ($(this).is(':visible')) $('this').css('display','flex'); });
+
+});
